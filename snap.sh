b613a4be761d443c4608e116e0876d1130731f81
paperbenni <paperbenni@gmail.com>
2020-04-29 20:58:26 +0200

    init

---
+++ snap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source /usr/share/paperbash/import.sh
+pb dialog
+messagebox "warning: snaps come with significantly higher resource and storage usage. \
+They also put no restrictions on low quality or broken applications and have no way to distinguish them from working programs. \
+use only if you have to. "
+
+if ! confirm "are you sure you want to enable snap support?"; then
+    clear
+    echo "good choice, but that's just my opinion..."
+    exit
+fi
+
+sudo pacman -R --noconfirm snap-dummy
+sudo pacman -S --noconfirm snapd
+sudo systemctl start snapd.socket
+sudo systemctl enable snapd.socket
+
+if confirm "a reboot is required. would you like to reboot now?"; then
+    reboot
+else
+    echo "snap installation done"
+fi
